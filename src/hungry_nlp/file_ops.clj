7d7d3b02ad2a64fada87d47d47ebfdc7cbe13bb7
Allen Kim <allenkim67@gmail.com>
2015-12-07 17:29:33 +0700

    use underscores instead of dashes

--- src/hungry_nlp/file_ops.clj
+++ src/hungry_nlp/file_ops.clj
@@ -8,14 +8,14 @@
 (defn get-shared-entities [] (read-json "resources/json/shared/entities.json"))
 (defn get-shared-intents [] (read-json "resources/json/shared/intents.json"))
 
-(defn get-user-entities [id] (read-json (str "resources/json/user/" id "-entities.json")))
-(defn write-user-entities [id content] (util/make-spit (str "resources/json/user/" id "-entities.json") (s/serialize content :json)))
+(defn get-user-entities [id] (read-json (str "resources/json/user/" id "_entities.json")))
+(defn write-user-entities [id content] (util/make-spit (str "resources/json/user/" id "_entities.json") (s/serialize content :json)))
 
-(defn entities-training-filepath [id] (str "resources/training/" id "-entities.train"))
-(defn intents-training-filepath [id] (str "resources/training/" id "-intents.train"))
+(defn entities-training-filepath [id] (str "resources/training/" id "_entities.train"))
+(defn intents-training-filepath [id] (str "resources/training/" id "_intents.train"))
 
-(defn write-entities-training [id content] (util/make-spit (str "resources/training/" id "-entities.train") content))
-(defn write-intents-training [id content] (util/make-spit (str "resources/training/" id "-intents.train") content))
+(defn write-entities-training [id content] (util/make-spit (str "resources/training/" id "_entities.train") content))
+(defn write-intents-training [id content] (util/make-spit (str "resources/training/" id "_intents.train") content))
 
 (defn get-merged-entities [id]
   (merge (get-shared-entities)
